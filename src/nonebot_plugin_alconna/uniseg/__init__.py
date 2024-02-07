a9a8ee00b93a799270591e69525ee993bdeda9a6
Tarrailt <rf_tar_railt@qq.com>
2024-02-07 15:20:02 +0000

    :bookmark: version 0.36.3

--- src/nonebot_plugin_alconna/uniseg/__init__.py
+++ src/nonebot_plugin_alconna/uniseg/__init__.py
@@ -35,7 +35,7 @@ from .fallback import FallbackSegment as FallbackSegment
 from .params import UniversalMessage as UniversalMessage
 from .params import UniversalSegment as UniversalSegment
 
-__version__ = "0.36.2"
+__version__ = "0.36.3"
 
 __plugin_meta__ = PluginMetadata(
     name="Universal Segment 插件",
