4eea08554fd42f1a5adb2d106daad71908565b3c
RF-Tar-Railt <rf_tar_railt@qq.com>
2024-09-04 01:55:41 +0800

    :white_check_mark: test_lang_list

--- tests/test_builtin_plugin.py
+++ tests/test_builtin_plugin.py
@@ -1,6 +1,7 @@
 import pytest
 from nonebug import App
 from nonebot import get_adapter
+from pytest_mock import MockerFixture
 from nonebot.adapters.satori import Bot, Adapter, Message, MessageSegment
 
 from tests.fake import FAKE_SATORI_LOGIN, fake_message_event_satori
@@ -83,55 +84,48 @@ async def test_lang_switch(app: App):
     async with app.test_matcher() as ctx:
         adapter = get_adapter(Adapter)
         bot = ctx.create_bot(base=Bot, adapter=adapter, login=FAKE_SATORI_LOGIN, info=None)
-        event1 = fake_message_event_satori(message=Message("/lang switch zh-CN"), id=123)
+        event1 = fake_message_event_satori(message=Message("/lang switch en-US"), id=123)
         ctx.receive_event(bot, event1)
-        ctx.should_call_send(event1, Message("切换语言成功: 'zh-CN'。"))
+        ctx.should_call_send(event1, Message("Switch to 'en-US' successfully."))
         ctx.should_finished()
-        assert lang.current == "zh-CN"
 
-
-# @pytest.mark.asyncio()
-# async def test_lang_list_success(app: App):
-#     from tarina.lang import _LangConfigData, lang
-
-#     from nonebot_plugin_alconna import load_builtin_plugin
-
-#     load_builtin_plugin("lang")
-
-#     lang.__dict__["_LangConfig__configs"]["test-lang"] = _LangConfigData(
-#         default="zh-CN", frozen={}, require={}, name=None, locales={"zh-CN"}
-#     )
-
-#     async with app.test_matcher() as ctx:
-#         adapter = get_adapter(Adapter)
-#         bot = ctx.create_bot(base=Bot, adapter=adapter, login=FAKE_SATORI_LOGIN, info=None)
-#         lang.select("zh-CN")
-#         event = fake_message_event_satori(message=Message("/lang list test-lang"), id=123)
-#         ctx.receive_event(bot, event)
-#         ctx.should_call_send(
-#             event,
-#             Message("""\
-# 支持的语言列表:
-#  * zh-CN"""),
-#         )
-#         ctx.should_finished()
+    assert lang.current == "en-US"
 
 
 @pytest.mark.asyncio()
-async def test_lang_list_failure(app: App):
+async def test_lang_list(app: App, mocker: MockerFixture):
     from tarina.lang import lang
 
     from nonebot_plugin_alconna import load_builtin_plugin
 
     load_builtin_plugin("lang")
 
+    old_locales_in = lang.locales_in
+
+    def locales_in(config_name: str):
+        if config_name == "test-lang":
+            return {"zh-CN"}
+        return old_locales_in(config_name)
+
+    mocker.patch("tarina.lang.lang.locales_in", locales_in)
+
     async with app.test_matcher() as ctx:
         adapter = get_adapter(Adapter)
         bot = ctx.create_bot(base=Bot, adapter=adapter, login=FAKE_SATORI_LOGIN, info=None)
         lang.select("zh-CN")
-        event = fake_message_event_satori(message=Message("/lang list 404"), id="123")
+        event = fake_message_event_satori(message=Message("/lang list test-lang"), id=123)
         ctx.receive_event(bot, event)
         ctx.should_call_send(
-            event, Message((MessageSegment.at("1"), MessageSegment.text(" 未能找到 404 所属的 i18n 目录")))
+            event,
+            """\
+支持的语言列表:
+ * zh-CN""",
+        )
+        ctx.should_finished()
+
+        event1 = fake_message_event_satori(message=Message("/lang list 404"), id="123")
+        ctx.receive_event(bot, event1)
+        ctx.should_call_send(
+            event1, Message((MessageSegment.at("1"), MessageSegment.text(" 未能找到 404 所属的 i18n 目录")))
         )
         ctx.should_finished()
